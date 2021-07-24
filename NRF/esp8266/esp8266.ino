32dac6dca67d4481d1fdcb0de05f215d5640a1cc
Dwi Setia Fardhana <84776462+CallmeSetia@users.noreply.github.com>
2021-07-24 22:39:48 +0700

    UPDATED

--- NRF/esp8266/esp8266.ino
+++ NRF/esp8266/esp8266.ino
@@ -6,11 +6,15 @@ int incomingByte = 0;
 // WIFI ESP8266
 #include <ESP8266WiFi.h>
 
-const char* SSID     = "polinema_hotspot_2G";
-const char* PASSWORD = "";
+const char* SSID     = "AndroidAPF642";
+const char* PASSWORD = "ydnerkusuma098";
 
 int statusWifi = 0;
+
 // HTTP
+// A 5rx - D2tx, 6tx - D1rx
+// N
+
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
@@ -19,6 +23,7 @@ String HOST = "projek.bengkelti.com";
 String TARGET = "pju";
 
 String URL = PROTOKOL + "://" + HOST + "/" + TARGET + "";
+uint8_t x = 0;
 String read1, read2, read3, read4, read5, read6;
 
 // Sofware Serial
@@ -31,24 +36,23 @@ SoftwareSerial terima(terima_RX, terima_TX); // RX, TX
 void setup() {
 
   Serial.begin(57600); // Baudrate Harus Sama
-  terima.begin(9600);
+  terima.begin(115200);
 
   Serial.println("Begin!");
 
   PROTOTHREAD_START();
   WIFI_START();
-
 }
 
 void loop() {
 
   cekStatusWifi(&WifiChecker, 10000);
+  //  x++;
+  //  Serial.println("TEST" + String(x));
 
 
-  // Cek Ada Data Incoming
   if ( Serial.available() > 1 ) {
-
-    String Read =  terima.readString(); //t#33#1.80#3.30
+    String Read =  Serial.readString(); //t#33#1.80#3.30
     read1 = parseString(Read, "#", 0);
 
     if (read1 == "t") {
@@ -56,38 +60,62 @@ void loop() {
       read2 = parseString(Read, "#", 1);
       read3 = parseString(Read, "#", 2);
       read4 = parseString(Read, "#", 3);
+
+      Serial.print("READ SERIAL : " + String(read2.toInt())) ;
+    }
+
+  }
+
+  // Cek Ada Data Incoming
+  if ( terima.available() > 1 ) {
+    //    Serial.println("1");
+
+    //    String terimaData = terima.readString();
+    //    Serial.println(terimaData);
+    //
+    //    String Read =  terima.readString(); //t#33#1.80#3.30
+    String Read =  terima.readString(); //t#33#1.80#3.30
+    read1 = parseString(Read, "#", 0);
+    Serial.println ("RECEIVE DATA : " + String(Read) );
+    if (read1 == "t") {
+
+      read2 = parseString(Read, "#", 1);
+      read3 = parseString(Read, "#", 2);
+      read4 = parseString(Read, "#", 3);
       read5 = parseString(Read, "#", 4);
       read6 = parseString(Read, "#", 5);
-      
+
       int rcwl = read2.toInt();
-      
+
       float teganganPV = read3.toFloat();
       float teganganBeban = read4.toFloat();
-      
+
       float arusPV = read5.toFloat();
       float arusBeban = read6.toFloat();
-      
+
       // http://projek.bengkelti.com/pju/dariESP.php?rcwlValue=24&teganganValue=2&dayaValue=4&arusValue=3&arusBeban=4&teganganBeban=5
-      
+
       String requestData = URL +  "/dariESP.php?rcwlValue=" + String(rcwl)
                            + "&teganganValue=" + String(teganganPV) +
                            +  "&dayaValue=" + String(arusPV * teganganPV ) +
                            +  "&arusValue=" + String(arusPV) +
-                           +  "&arusBeban=" + String(arusBeban) + 
+                           +  "&arusBeban=" + String(arusBeban) +
                            +  "&teganganBeban=" + String(teganganBeban) + "";
 
       // ---
       // Debug
       // ---
 
-      //      Serial.println("RCWL : " + String(rcwl) + " - Tegangan : " + String(tegangan) + " - Arus : " + String(arus) + + " - Daya : " + String(daya) + " - END " );
-      //      delay(100);
-      
+      // Serial.println("RCWL : " + String(rcwl) + " - Tegangan : " + String(tegangan) + " - Arus : " + String(arus) + + " - Daya : " + String(daya) + " - END " );
+      // delay(100);
+
       // ----
       // kirim ke database
       // ----
 
       if (WiFi.status() == WL_CONNECTED) {
+
+        Serial.println("SEND");
         WiFiClient client;
         HTTPClient http;
 
